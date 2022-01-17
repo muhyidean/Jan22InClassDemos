f3e20e0bf5feb5f553b558ab65dd83e24b2f8897
Muhyieddin <muhyidean@gmail.com>
2022-01-17 14:28:19 -0600

    Merge remote-tracking branch 'origin/master'

---
+++ src/lecture04_05/Student.java
@@ -0,0 +1,41 @@
+package lecture04_05;
+
+public class Student {
+
+    private int id;
+    private String name;
+    private double GPA;
+
+    public Student(int id, String name, double GPA) {
+        this.id = id;
+        this.name = name;
+        this.GPA = GPA;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(double GPA) {
+        this.GPA = GPA;
+    }
+
+
+
+}
